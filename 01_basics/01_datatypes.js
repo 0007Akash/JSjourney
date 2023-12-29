f30bf6b2200ac7603d904b91578068d37ce9889c
0007Akash <143246301+0007Akash@users.noreply.github.com>
2023-12-29 10:39:59 +0000

    variables and datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat older code to newer version of js
+
+// alert("Hello") this happens in browser
+
+// primitive datatypes
+// number
+// bigint
+// string => ""
+// boolean true/false
+// null      standalone value
+// undefined 
+// symbol => to identify uniqueness
+
+console.log(typeof "Akash Yadav"); // string datatype
+
+console.log(typeof 6); //number datatypes
+
+console.log(typeof null); //object type
+
+console.log(typeof undefined); // undefined type
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="infoakash0113@gmail.com"
+var accountPassword="12345"
+
+accountCity="Jaipur"
+
+// accountId=5   changes to const is not allowed
+
+accountEmail="hc0011@gmail.com"
+accountPassword="4344"
+accountCity="Bengaluru"
+/*
+prefer not to use var 
+because of the issue of the block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
