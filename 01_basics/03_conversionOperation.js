71eddd699b7020762014b6985a26b15ad9bceeef
0007Akash <143246301+0007Akash@users.noreply.github.com>
2023-12-29 11:06:31 +0000

    Datatypes Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let number=5
+
+let booleanNumber=Boolean(number)
+let stringNumber=String(number)
+
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// 5 => true in boolean
+// number can be converted into boolean , string 
+
+let string="6ac"
+
+let stringInNumber=Number(string)
+
+// console.log(typeof stringInNumber);
+// console.log(stringInNumber);
+
+// "6ac" => number but value is NaN-Not a Number
+// "akash" => true , "" => false
+
+let isLoggedIn=false
+
+let numberLoggedIn=Number(isLoggedIn)
+
+// console.log(typeof numberLoggedIn);
+// console.log(numberLoggedIn);
+
+// true => 1 & false => 0 in number
+
+let value=undefined
+
+let numberValue=Number(value)
+
+console.log(typeof numberValue);
+console.log(numberValue);
+
+// null => 0 in number & undefined => NaN
+
+
+
